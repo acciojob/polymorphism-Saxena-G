6388621114b9cf33b5c5b50c7e9903afbd7448b6
Gaurav Saxena <99750798+Saxena-G@users.noreply.github.com>
2024-09-12 14:50:03 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,16 +1,13 @@
 package com.driver;
 class Product {
     public int product(int x, int y) {
-        int product = x*y;
-        return product;
+     
     }
     public int product(int x, int y, int z) {
-        int product = x*y*z;
-        return product;
+        
     }
     public double product(double x, double y) {
-        double product = x*y;
-        return product;
+      
     }
 }
 public class Main {
